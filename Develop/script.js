99c258af2f44f1223597507193f6b4825c39dc32
Jesse De Jong <jdoceau@gmail.com>
2021-06-09 18:47:08 +1000

    Started work on password randomiser function

--- Develop/script.js
+++ Develop/script.js
@@ -18,34 +18,51 @@ var validationNeg = ["n", "no"];
 
 var valid = false;
 var userInput = "";
+var length = "";
 var passwordIncludes = [];
 
 // User prompts
 var Prompt = "";
+var Prompt2 = "";
+
+// Password Construction Arrays
+var lowercase = ["abcdefghijklmnopqrstuvwxyz"];
+var UPPERCASE = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var Numerical = ["1234567890"];
+var Special = ["!@#$%^&*()-+_=,./?~"];
 
 // Generate Password Function
 function generatePassword() {
+  // Collect user input for what to include in the password
   lengthValidationLoop();
   Prompt = "lowercase";
+  Prompt2 = lowercase;
   validationLoop();
   Prompt = "UPPERCASE";
+  Prompt2 = UPPERCASE;
   validationLoop();
   Prompt = "Numerical";
+  Prompt2 = Numerical;
   validationLoop();
   Prompt = "Special";
+  Prompt2 = Special;
   validationLoop();
   console.log(passwordIncludes);
+  passwordGen();
+
+  // Generate the password from the user selected criteria
+
 }
 
 // (Length prompt) Reusable validation loop for user input
-var lengthValidationLoop = function (userInput, Promt) {
+var lengthValidationLoop = function (userInput) {
   // Create loop (100 Iterations)
   for (i = 0; i < 100; i++) {
     var userInput = window.prompt("What is the desired length of the password [8 to 128] Characters?");
   
    if (userInput >= 8 && userInput <= 128) {
      valid = true;
-     passwordIncludes.push(userInput);
+     length = userInput;
      break;
    } 
  }
@@ -53,7 +70,7 @@ var lengthValidationLoop = function (userInput, Promt) {
  // reset validation toggle
   valid = false;
   userInput = "";
-}
+};
 
 // Reusable validation loop for user input
 var validationLoop = function (userInput, Promt) {
@@ -64,7 +81,7 @@ var validationLoop = function (userInput, Promt) {
  userInput = userInput.toLowerCase();    
    if (validation.includes(userInput)) {
      valid = true;
-     passwordIncludes.push(Prompt);
+     passwordIncludes.push(Prompt2);
      break;
    } else if (validationNeg.includes(userInput)) {
     valid = true;
@@ -75,4 +92,18 @@ var validationLoop = function (userInput, Promt) {
   valid = false;
   userInput = "";
 }
+
+var passwordGen = function (passwordIncludes) {
+  // Run the loop for the desired length selected by the user
+  for (i = 0; i < length; i++) {
+    // Get random index from array passwordIncludes
+    var index = Math.floor(Math.random() * length);
+    var option = passwordIncludes[index];
+    console.log(index, option);
+    // Get random index from the randomly selected array 
+    var index = Math.floor(Math.random() * option.length);
+    console.log(index);
+  }
+};
+
 };
